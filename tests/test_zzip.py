fce3f30df3aa305debe2a2d4dc23044d4c17008b
Stephen Mizell <smizell@gmail.com>
2021-11-12 14:09:03 -0600

    Support selectors

--- tests/test_zzip.py
+++ tests/test_zzip.py
@@ -3,7 +3,9 @@ from zzip import zipper
 
 def test_list_down():
     z = zipper([1, 2, 3])
-    assert z.down().current == 1
+    zd = z.down()
+    assert zd.current == 1
+    assert zd.path.index == 0
 
 
 def test_list_down_index():
@@ -37,5 +39,80 @@ def test_list_replace():
 
 
 def test_advanced():
-    z = zipper([1, [2, 3], 4])
-    assert z.down().right().down().replace(200).top().current == [1, [200, 3], 4]
+    z = zipper([1, [2, [3, 4]]])
+    assert z.down().right().down().right().down().replace(300).top().current == [
+        1,
+        [2, [300, 4]],
+    ]
+
+
+def test_dict_down():
+    z = zipper({"a": 1})
+    zd = z.down()
+    assert zd.current == 1
+    assert zd.path.key == "a"
+
+
+def test_dict_down_down():
+    z = zipper({"a": {"b": 2}})
+    zd = z.down().down()
+    assert zd.current == 2
+    assert zd.path.key == "b"
+
+
+def test_dict_down_key():
+    z = zipper({"a": 1, "b": 2, "c": 3})
+    zd = z.down("c")
+    assert zd.current == 3
+    assert zd.path.key == "c"
+
+
+def test_dict_right():
+    z = zipper({"a": 1, "b": 2})
+    zv = z.down().right()
+    assert zv.current == 2
+    assert zv.path.key == "b"
+
+
+def test_dict_left():
+    z = zipper({"a": 1, "b": 2})
+    zv = z.down().right().left()
+    assert zv.current == 1
+    assert zv.path.key == "a"
+
+
+def test_dict_up():
+    z = zipper({"a": 1})
+    zv = z.down().up()
+    assert zv.current == {"a": 1}
+
+
+def test_dict_replace():
+    z = zipper({"a": 1})
+    assert z.down().replace(100).top().current == {"a": 100}
+
+
+def test_nested():
+    z = zipper({"a": {"b": 1, "c": [4, 5, 6]}})
+    assert z.down().down().right().down().replace(400).top().current == {
+        "a": {"b": 1, "c": [400, 5, 6]}
+    }
+    assert z.down().down().replace(100).top().current == {
+        "a": {"b": 100, "c": [4, 5, 6]}
+    }
+    # Make sure the original isn't altered
+    assert z.current == {"a": {"b": 1, "c": [4, 5, 6]}}
+
+
+# def test_multiple_replace():
+#     z = zipper({"a": {"b": 1, "c": [4, 5, 6]}})
+#     assert z.down().down().replace(100, persist=True).right().down().replace(
+#         400
+#     ).top().current == {"a": {"b": 100, "c": [400, 5, 6]}}
+
+
+def test_selector():
+    z = zipper({"a": {"b": 1, "c": [4, 5, 6]}})
+    zv = z.select(["a", "c", 1])
+    assert zv.current == 5
+    assert zv.path.selector == ("a", "c", 1)
--- zzip/__init__.py
+++ zzip/__init__.py
@@ -10,7 +10,49 @@ class Path:
     right: Tuple
     parent_nodes: Tuple
     parent_path: Tuple
-    changed: bool = False
+    selector: Tuple
+    changed: bool
+    index: int
+
+    def mark_changed(self):
+        return replace(self, changed=True)
+
+
+@dataclass
+class ListPath(Path):
+    def new_current(self, current):
+        return [*self.left, current, *self.right]
+
+
+@dataclass
+class DictPath(Path):
+    keys: List[str]
+
+    @property
+    def key(self):
+        return self.keys[self.index]
+
+    def parent_node(self):
+        return self.parent_nodes[-1]
+
+    def go_right(self, current):
+        new_current = self.parent_node()[self.right[0]]
+        right = self.right[1:]
+        left = (*self.left, self.key)
+        path = replace(self, index=len(left), left=left, right=right)
+        return new_current, path
+
+    def go_left(self, current):
+        new_current = self.parent_node()[self.left[-1]]
+        left = self.left[:-1]
+        right = (self.key, *self.right)
+        path = replace(self, index=len(left), left=left, right=right)
+        return new_current, path
+
+    def new_current(self, current):
+        new_current = dict(**self.parent_node())
+        new_current[self.key] = current
+        return new_current
 
 
 @dataclass
@@ -22,15 +64,44 @@ class Location:
         if isinstance(self.current, list):
             list_goto = goto or 0
             items = self.current
-            path = Path(
-                left=items[:list_goto],
+            left = items[:list_goto]
+            index = len(left)
+            selector = (*self.path.selector, index) if self.path else (index,)
+            path = ListPath(
+                index=index,
+                selector=selector,
+                left=left,
                 right=items[list_goto + 1 :],
                 parent_nodes=(*self.path.parent_nodes, self.current)
                 if self.path
                 else (self.current,),
                 parent_path=self.path,
+                changed=False,
             )
             return replace(self, current=items[list_goto], path=path)
+        if isinstance(self.current, dict):
+            keys = list(self.current.keys())
+            if not keys:
+                # todo: raise instead
+                return
+            current_key = goto or keys[0]
+            index = keys.index(current_key)
+            selector = (
+                (*self.path.selector, current_key) if self.path else (current_key,)
+            )
+            path = DictPath(
+                keys=keys,
+                index=index,
+                selector=selector,
+                left=keys[:index],
+                right=keys[index + 1 :],
+                parent_nodes=(*self.path.parent_nodes, self.current)
+                if self.path
+                else (self.current,),
+                parent_path=self.path,
+                changed=False,
+            )
+            return replace(self, current=self.current[current_key], path=path)
 
     def up(self):
         if self.path:
@@ -39,9 +110,9 @@ class Location:
                 if self.path.changed:
                     return replace(
                         self,
-                        current=[*self.path.left, self.current, *self.path.right],
+                        current=self.path.new_current(self.current),
                         path=self.path.parent_path
-                        and replace(self.path.parent_path, changed=True),
+                        and self.path.parent_path.mark_changed(),
                     )
                 return replace(self, current=parent_node, path=self.path.parent_path)
         # todo: elses
@@ -49,12 +120,21 @@ class Location:
     def right(self):
         # todo: fail when no right
         if self.path and self.path.right:
+            # todo: make work with ListPath, too
+            if isinstance(self.path, DictPath):
+                current, path = self.path.go_right(self.current)
+                return replace(self, current=current, path=path)
             current, right = self.path.right[0], self.path.right[1:]
-            path = replace(self.path, left=(*self.path.left, self.current), right=right)
+            left = (*self.path.left, self.current)
+            path = replace(self.path, index=len(left), left=left, right=right)
             return replace(self, current=current, path=path)
 
     def left(self):
         if self.path and self.path.left:
+            # todo: make work with ListPath, too
+            if isinstance(self.path, DictPath):
+                current, path = self.path.go_left(self.current)
+                return replace(self, current=current, path=path)
             current, left = self.path.left[-1], self.path.left[:-1]
             path = replace(self.path, left=left, right=(self.current, *self.path.right))
             return replace(self, current=current, path=path)
@@ -65,10 +145,16 @@ class Location:
             loc = loc.up()
         return loc
 
-    def replace(self, value):
+    def replace(self, value, persist=False):
         if self.path:
             return replace(self, current=value, path=replace(self.path, changed=True))
         return replace(self, current=value)
+
+    def select(self, selector):
+        loc = self
+        for s in selector:
+            loc = loc.down(s)
+        return loc
 
 
 def zipper(value):
